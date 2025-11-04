18f4c6fd0e00e78316037c13458f14a6e568ad07
527mbeard <527mbeard@frhsd.com>
2025-11-04 10:43:02 -0500

    javascript done, do css

--- app.js
+++ app.js
@@ -20,17 +20,38 @@
    - Name variables clearly so you remember what each one is for.
 */
 
-// let ________ = prompt("Ask the user for ________");
-// let ________ = prompt("Ask the user for ________");
-// (Add as many as your story needs)
+let userName = prompt("What is the main character's name?");
+let sidekick = prompt("What is the sidekick's name?");
+let place = prompt("Where does your story take place?");
+let adjective = prompt(" Give me an adjective?");
+let adjective2 = prompt("Give me another adjective?");
+let animal= prompt("What is ur fav animal?");
+let verbEndingInIng = prompt("Give me a verb ending in -ing?");
+let food = prompt("What is ur fav food?");
+let emotion = prompt("Give me an emotion?");
+let verb = prompt("Give me a verb: that does`t end in -ing?");
+let someone = prompt("Give me a name of a celebrity?");
+let vehicle = prompt("Give me a vehicle?");
+let verb2 = prompt("Give me another verb: that does`t end in -ing?");
+let emotion2 = prompt("Give me another emotion?");
+let action = prompt("Give me a exercise that starts with do a?");
+let basic = prompt("Give me a basic adjective?");
 
 /* 2) BUILD YOUR STORY STRING
    - Use a template literal: it starts and ends with backticks (the ` key).
    - Insert variables with ${variableName} in your sentence.
    - Keep it fun! Make sure your story uses ALL the variables you collected.
 */
+let story = `<p>One day ${userName} and ${sidekick} went to ${place}.; </p>
+<p> ${userName} was really ${adjective} and ${adjective2}.;</p>
+<p> Suddenly, they saw a ${animal} that was ${verbEndingInIng} near a pile of ${food}.;</p>
+<p> They were very ${emotion} because that was their favorite food.;</p>
+<p> ${userName} and ${sidekick} decided to ${verb} the ${animal}.;</p>
+<P> ${someone} driving past them, saw this and stopped their ${vehicle} to ${verb2}.;</p>
+<p> ${userName} was very ${emotion2} about this and decided to ${action};</p>
+<P> The ${someone} thought this was really ${basic} and put them on social media.;</p>
+<p> They became famous! The end. ;</p>` 
 
-// let story = `Write your story here using ${_____} and ${_____} etc.`;
 
 /* 3) DISPLAY THE STORY ON THE PAGE
    - Select the element with id "madlib-output" using document.getElementById(...)
@@ -38,7 +59,7 @@
    - Optional: include simple <strong> or <em> tags inside your story string for emphasis.
 */
 
-// document.getElementById("madlib-output").innerHTML = /* your story variable */ ;
+ document.getElementById("madlib-output").innerHTML = story ;
 
 /* 4) OPTIONAL POLISH
    - Add a title or intro line at the top of your story (still using the same innerHTML).
